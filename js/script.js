bc347da75a5f15dff371ea3c48e0013d8cba9fd4
johnnyvi26 <johnnymv26@gmail.com>
2021-09-16 16:58:27 -0400

    connecting to the API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log($);
+const $characterName = $('#characterName');
+const $characterImg = $('#characterImg');
+const $powers = $('#powers');
+const $characterBio = $('#characterBio');
+const $characterInfo = $('#characterInfo');
+const $input = $('input[type="text"]');
+
+
+function HandleGetData(evt) {
+    nameInput = $input.val();
+
+    $.ajax({
+        url: `https://superheroapi.com/api/4726525207391640/search/${nameInput}`
+    }).then(
+        (data) => {
+            console.log(data);
+            render(data);
+        },
+        (error) => {
+            console.log("Oops something went wrong: ", error);
+        }
+    );
+}
+
